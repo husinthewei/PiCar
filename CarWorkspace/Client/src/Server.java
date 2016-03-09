c056837380ad755a79e20b5fea0a73597d21910e
Weiwei Hu <wae3wae@yahoo.com>
2016-03-08 22:35:39 -0500

    DoneStage1
    Ready for MCSTA

--- CarWorkspace/Client/src/Server.java
+++ CarWorkspace/Client/src/Server.java
@@ -2,6 +2,7 @@ import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Image;
+import java.awt.Point;
 import java.awt.RenderingHints;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
@@ -18,13 +19,12 @@ import javax.swing.JPanel;
 
 class Server extends JPanel implements KeyListener, MouseListener, MouseMotionListener{
 	
-//THis one is better
-public static DatagramSocket serverSocket;
-public static InetAddress IPAddress;
+public static DatagramSocket serverSocket;  
+public static InetAddress IPAddress; 
 public static int port;
-public static int[] movements = {0,0,0,0};
+public static int[] movements = {0,0,0,0}; //Holds the movement data that will be sent
 
-	public static void createFrame(Server g) throws IOException{
+	public static void createFrame(Server g) throws IOException{  //creating the GUI
 	JFrame frame = new JFrame("Car GUI");
 	frame.add(g);
 	frame.setSize(440, 590);
@@ -36,7 +36,7 @@ public static int[] movements = {0,0,0,0};
 	frame.setVisible(true);
 	}
 
-	public static void SendSomething(String something) throws IOException{
+	public static void SendSomething(String something) throws IOException{ //send the data
 		byte[] sendData = new byte[1024];
 		sendData = something.getBytes();
 		DatagramPacket sendPacket = new DatagramPacket(sendData,sendData.length, IPAddress, port);
@@ -45,47 +45,39 @@ public static int[] movements = {0,0,0,0};
 	
 	public static void main(String args[]) throws Exception {
 		Server demo = new Server();
-		createFrame(demo);
-		serverSocket = new DatagramSocket(3141);
+		createFrame(demo); //creating the frame
+		serverSocket = new DatagramSocket(3141); //socket on port 3141
 		byte[] receiveData = new byte[1024];
 		while (true) {
-			demo.repaint();
-			
-			//System.out.println(movements[0] + " " + movements[1] + " " + movements[2] + " " + movements[3]);
-			
-			DatagramPacket receivePacket = new DatagramPacket(receiveData,
+			demo.repaint();	//update the GUI
+			DatagramPacket receivePacket = new DatagramPacket(receiveData, 
 					receiveData.length);
-			serverSocket.receive(receivePacket);
+			serverSocket.receive(receivePacket);   //get some data
 			String sentence = new String(receivePacket.getData());
-			//System.out.println("RECEIVED: " + sentence);
-			IPAddress = receivePacket.getAddress();
+			IPAddress = receivePacket.getAddress(); //saving the address, to reply to
 			port = receivePacket.getPort();
-			//String capitalizedSentence = sentence.toUpperCase();
-			//protocol:
-			//forwards/backwards/right/left
-			//Ex: 1011
-			String msg = movements[0] + "" + movements[1] + "" + movements[2] + "" + movements[3];
-			SendSomething(msg);
+			String msg = movements[0] + "" + movements[1] + "" + movements[2] + "" + movements[3]; //forwards/backwards/right/left
+			SendSomething(msg); //send the data
 			}
 	}
 
-	
 	public void paint(Graphics g){
 		Graphics2D g2d = (Graphics2D) g;
 		paintComponent(g);
 		g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING,RenderingHints.VALUE_ANTIALIAS_ON);
 		
-		//drawing background
+		//drawing background color
 		g2d.setColor(Color.DARK_GRAY);
 		g2d.fillRect(0,0,700,700);
 		
 		//drawing directions
 		g2d.setColor(Color.WHITE);
-		g2d.drawString("Use wasd to move", 155, 400);
-		g2d.drawString("   ^   ", 185, 424);
-		g2d.drawString("<- | ->", 181, 430);
-		g2d.drawString("   v    ", 185, 440);
+		g2d.drawString("Use wasd to move", 155, 380);
+		g2d.drawString("   ^   ", 185, 404);
+		g2d.drawString("<- | ->", 181, 410);
+		g2d.drawString("   v    ", 185, 420);
 		
+		//fetching image and drawing it to GUI
 		BufferedImage image = null;
 		try {
 		    URL url = new URL("http://192.168.1.11:8080/?action=snapshot&n=401");
@@ -93,10 +85,10 @@ public static int[] movements = {0,0,0,0};
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+			
 		g2d.drawImage(image,10,10,414,240,null);
-
+      
 	}
-	
 	
 	@Override
 	public void mouseDragged(MouseEvent arg0) {
@@ -142,27 +134,26 @@ public static int[] movements = {0,0,0,0};
 
 	@Override
 	public void keyPressed(KeyEvent arg0) {
-		if(arg0.getKeyCode() == 87)
-			movements[0] = 1;
-		if(arg0.getKeyCode() == 83)
+		if(arg0.getKeyCode() == 87) //up
+			movements[0] = 1; //change "forward variable" to 1
+		if(arg0.getKeyCode() == 83) //down
 			movements[1] = 1;		
-		if(arg0.getKeyCode() == 65)
+		if(arg0.getKeyCode() == 65) //left
 			movements[2] = 1;	
-		if(arg0.getKeyCode() == 68)
-			movements[3] = 1;		
+		if(arg0.getKeyCode() == 68) //right
+			movements[3] = 1;	
 	}
 
 	@Override
 	public void keyReleased(KeyEvent arg0) {
 		if(arg0.getKeyCode() == 87)
-			movements[0] = 0;
+			movements[0] = 0; //change "forward variable" to 0
 		if(arg0.getKeyCode() == 83)
 			movements[1] = 0;		
 		if(arg0.getKeyCode() == 65)
 			movements[2] = 0;	
 		if(arg0.getKeyCode() == 68)
-			movements[3] = 0;	
-		
+			movements[3] = 0;		
 	}
 
 	@Override
--- Client.py
+++ Client.py
@@ -3,23 +3,35 @@ import time
 import select
 import RPi.GPIO as GPIO
 
-#UDP_IP = "10.170.67.252" (school)
-UDP_IP = "192.168.1.9"
+#UDP_IP = "10.170.67.252" (school IP)
+UDP_IP = "192.168.1.10"
 UDP_PORT = 3141
 MESSAGE = "REQ"
-#http://www.bytecreation.com/blog/2013/10/13/raspberry-pi-ultrasonic-sensor-hc-sr04
 
-A1 = 7
-A2 = 8
+A1 = 7 #left gate1 (H-bridge)
+A2 = 8 #left gate2 (H-bridge)
 
-A3 = 23
-A4 = 24
+A3 = 23 #right gate1 (H-bridge)
+A4 = 24 #right gate2 (H-bridge)
+
+#Front: e=17 t=21
+#Back: e=10 t=9
+EchoFront = 17 #for utlrasonic sensors
+TrigFront = 27
+EchoBack = 10
+TrigBack = 9
+
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 GPIO.setup(A1,GPIO.OUT)
 GPIO.setup(A2,GPIO.OUT)
 GPIO.setup(A3,GPIO.OUT)
 GPIO.setup(A4,GPIO.OUT)
+
+GPIO.setup(TrigFront,GPIO.OUT)
+GPIO.setup(EchoFront,GPIO.IN)
+GPIO.setup(TrigBack,GPIO.OUT)
+GPIO.setup(EchoBack,GPIO.IN)
 
 print "UDP target IP:", UDP_IP
 print "UDP target port:", UDP_PORT
@@ -64,7 +76,6 @@ def UDPCom():
     ready = select.select([sock], [], [], 2)
     if ready[0]: 
         data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
-        #print "received message:", data   
         return data
     else:
         return "nothing received"   
@@ -83,8 +94,35 @@ def analyzeIn(msg):
     if(msg[3:4] == "1"):
         right()
         
-while(True):
-    msg = UDPCom()
+def getDistance(echo, trig):
+    GPIO.output(trig, True)     #triggering signal
+    time.sleep(0.00001)
+    GPIO.output(trig, False)
+    signaloff = 0; signalon = 0;
+    
+    i = 0;
+    while GPIO.input(echo) == 0 and i <300:     #If more than 300 readings (lost signal), cancel
+          signaloff = time.time()                
+          i+=1
+    i=0;
+    while GPIO.input(echo) == 1 and i <300:    # measuring return signal
+          signalon = time.time()
+          i+=1
+    timepassed = signalon - signaloff    #difference in time
+    distance = timepassed * 17000 #in cm  
+    return distance
+
+def doDistanceFeature():
+    a = getDistance(EchoFront, TrigFront)    #reading sensor
+    b = getDistance(EchoBack, TrigBack)      #reading sensor
+    if(b < 20):         #if object is closer than 20 front or back, move away
+        back()
+    if(a < 20):        
+        forward()
+        
+while(True):             #main loop. Cycles every 0.1 seconds
+    msg = UDPCom()       #gets the message. Has a 2 second timeout.
     print "received message:", msg
-    analyzeIn(msg) 
-    time.sleep(0.10) 
+    analyzeIn(msg)       #Interprets message to move if necessary
+    doDistanceFeature()  #Using ultrasonic sensor to prevent "crashes"
+    time.sleep(0.1) 
